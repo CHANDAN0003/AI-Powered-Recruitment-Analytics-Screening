000832f23208333862cc3029cccf4d2d87b8f6fe
chandan Kumar <skumargouda76@gmail.com>
2025-10-19 11:22:00 +0530

    Updated code

--- public/chatbot.js
+++
@@ -1,2 +0,0 @@
-// REMOVED: legacy file neutralized. This file has been disabled and is no longer used by the project.
-// If you need it removed from the repository entirely, run a git rm or delete it locally.
